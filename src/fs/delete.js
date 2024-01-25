75c026254e47fceea6d0278d5405555e7fe71a49
maximiloi <maximiloi@gmail.com>
2024-01-25 17:19:14 +0300

    feat: task delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { unlink } from 'fs/promises';
+
+const FOLDER_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToRemove = join(__dirname, FOLDER_NAME, 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await unlink(fileToRemove);
+    console('file remove success');
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await remove();
