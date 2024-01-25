dbaf1f95b1d809f5701559a60ec379dff552f7b0
maximiloi <maximiloi@gmail.com>
2024-01-25 13:50:07 +0300

    feat: create task callback

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { writeFile, stat } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const TEXT = 'I am fresh and young';
+  const FILE_NAME = 'fresh.txt';
+  const ERROR_MESSAGE = 'FS operation failed';
+
+  const filePath = join(__dirname, 'files', FILE_NAME);
+
+  stat(filePath, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        writeFile(filePath, TEXT, (err) => {
+          if (err) console.log(err);
+          else console.log(`file ${FILE_NAME} created success`);
+        });
+      }
+    } else {
+      console.log(ERROR_MESSAGE);
+    }
+  });
 };
 
 await create();
