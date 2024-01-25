2dad21a2fd89c5514da4cb453e096a15c99b95bb
maximiloi <maximiloi@gmail.com>
2024-01-25 16:42:39 +0300

    feat: task copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { cp } from 'fs/promises';
+
+const FOLDER_NAME = 'files';
+const FOLDER_COPY_NAME = 'files_copy';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const srcPath = join(__dirname, FOLDER_NAME);
+const destPath = join(__dirname, FOLDER_COPY_NAME);
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await cp(srcPath, destPath, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+    console.log('Copy folder success');
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await copy();
