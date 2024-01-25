8a3d42a2cd1b58e54c47e693b92088fbcc9003e5
maximiloi <maximiloi@gmail.com>
2024-01-25 17:35:19 +0300

    feat: task read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { readFile } from 'fs/promises';
+
+const FOLDER_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, FOLDER_NAME, 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    console.log(contents);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
