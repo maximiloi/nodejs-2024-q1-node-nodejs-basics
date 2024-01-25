f27aa2e85dbe9bbc540b7fb74ab94f1cd4fe24c6
maximiloi <maximiloi@gmail.com>
2024-01-25 17:28:37 +0300

    feat: task list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { readdir } from 'fs/promises';
+
+const FOLDER_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const folder = join(__dirname, FOLDER_NAME);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(folder);
+    console.log(files);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await list();
