c80562e28463635672eb028020a36ce5a13d5499
maximiloi <maximiloi@gmail.com>
2024-01-25 17:04:39 +0300

    feat: task rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import fs from 'fs/promises';
+
+const FOLDER_NAME = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const srcFile = join(__dirname, FOLDER_NAME, 'wrongFilename.txt');
+const destFile = join(__dirname, FOLDER_NAME, 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.rename(srcFile, destFile);
+    console.log('rename file success');
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await rename();
